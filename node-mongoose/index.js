72d7c052676f3382eea97e6398a5c6b4c07f58d8
coderprasukj <prasukjain0047@gmail.com>
2021-01-15 16:27:30 +0530

    Mongoose Part 3

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -14,11 +14,24 @@ connect.then((db) => {
     })
     .then((dish) => {
         console.log(dish);
-        return Dishes.find({}).exec();
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated Test' }
+        },{
+            new: true
+        }).exec();
     })
-    .then((dishes) => {
-        console.log(dishes);
-        return Dishes.remove();
+    .then((dish) => {
+        console.log(dish);
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        });
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
+        return Dishes.remove({});
     })
     .then(() => {
         return mongoose.Collection.close();
